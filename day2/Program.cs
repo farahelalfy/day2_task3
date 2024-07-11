d669e512d66f0614c88cdd4c4f6f996ad6315880
farah <farah@LAPTOP-RJCB7SM0>
2024-07-11 14:24:23 +0300

    Add project files.

---
+++ day2/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace day2
+{
+
+    class student
+    {
+        int ssn;
+        string name;
+        int age;
+        string phone;
+        public void getdata(int Ssn, string Name, int Age, string Phone = "")
+        {
+            ssn = Ssn;
+            name = Name;
+            age = Age;
+            phone = Phone;
+        }
+        public void print()
+        {
+            Console.WriteLine($"{ssn}-{name}-{age}-{phone}");
+        }
+
+
+    }
+
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            student farah = new student();
+            farah.getdata(11, "farah", 20, "012222222");
+            farah.print();
+
+        }
+
+
+    }
+}
+
